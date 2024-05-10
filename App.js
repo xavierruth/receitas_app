0b8b16a763583239ce234505dd78668238b28559
xavierruth <ruthmarianne.xavier@gmail.com>
2024-05-10 10:15:11 -0300

    conhecendo rn

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image style={styles.tiny}
+      source={{uri:'https://avatarfiles.alphacoders.com/239/239400.jpg'}}
+      />
+
+      <Text>Olá, Mari ♥</Text>
+      <Text>Idade: 27</Text>
+      <Text>Ocupação: Desenvolvedora de Software</Text>
+      <Text>Experiência: 5 anos de experiência em 
+        desenvolvimento web e móvel.</Text>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#EFF5FB',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  tiny:{
+    width: 90,
+    height: 90,
+    borderRadius: 90,
   },
 });
